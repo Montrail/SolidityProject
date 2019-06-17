aac1aba624f824c29f714bc43190fc27a9d2c7af
Montrail Cooper <Way2Automate@Montrails-MacBook-Pro.local>
2019-06-17 15:58:19 -0700

    Adds Archtectual testing environment using mocha

--- Inbox/compile.js
+++ Inbox/compile.js
@@ -5,6 +5,6 @@ const solc = require("solc");
 const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 const source = fs.readFileSync(inboxPath, "utf8"); //change to utf8 to UTF-8
 
-//console.log(solc.compile(source, 1));
+console.log(solc.compile(source, 1));
 
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+//module.exports = solc.compile(source, 1).contracts[':Inbox'];
